14e72339ea63afd54d538893e07a492f808564ab
Jaag <168704320+ja0011@users.noreply.github.com>
2026-08-20 03:13:52 -0500

    Add: python-string-concatenation - submission-2

---
+++ Python For Beginners/python-string-concatenation/submission-2.py
@@ -0,0 +1,15 @@
+def concatenate(s1: str, s2: str) -> str:
+    t = len(s1) + len(s2)
+
+    if t > 11:
+        return("Too long!")
+    else:
+        return (s1 +s2)
+
+
+
+
+# do not modify below this line
+print(concatenate("He", "llo"))
+print(concatenate("Hello ", "world!"))
+print(concatenate("Length", "of10"))
